ad015ed4e94a575546509446021ffcf136ed36be
Jakub Kaderka <jakub.kaderka@gmail.com>
2019-02-19 22:49:59 +0100

    Fixed gps driver transmit data

--- sw/src/drivers/gps.c
+++ sw/src/drivers/gps.c
@@ -161,7 +161,7 @@ void GPSd_Init(void)
     usart_set_baudrate(USART2, 9600);
     usart_set_databits(USART2, 8);
     usart_set_stopbits(USART2, USART_STOPBITS_1);
-    usart_set_mode(USART2, USART_MODE_RX);
+    usart_set_mode(USART2, USART_MODE_TX_RX);
     usart_set_parity(USART2, USART_PARITY_NONE);
     usart_set_flow_control(USART2, USART_FLOWCONTROL_NONE);
 
